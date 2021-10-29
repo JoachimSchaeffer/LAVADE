a4540cdef0f5492b5880210b4df1e275144a93ae
JoachimSchaeffer <63883039+JoachimSchaeffer@users.noreply.github.com>
2021-10-29 18:49:45 +0200

    Beta version

--- 01Code/Data.m
+++ 01Code/Data.m
@@ -1,53 +1,44 @@
 classdef Data < handle
     properties
-        % general properties!
-        name;
-        active_dataset;
-        X;
-        group; % underlying implicit group of the data. 
-        y;
+        % general properties of the class
+        name;           % Name of the dataset, assigned in LAVADE
+        active_dataset; % Name of the dataset currently laoded
+        X;              % X data
+        group;          % underlying implicit group of the data. 
+        y;              % modeling objective    
         X_train;
         X_test;
         y_train;
         y_test;
-        idx_para; % (random) index for train and test, respecting groups
-        x_label_text; 
-        y_label_text;
-        output_text; 
+        idx_para;       % (random) index for training and test set
+        x_label_text;   % x-axis label of the data X
+        y_label_text;   % y-xis label of the data X
+        output_text;    %
         x_values;
-        % properties on;ly neede for the example case, but its fine
+        % properties only neede for the example case, but its fine
         % if they are empty otherwise
-        start_value;     %  Mean of first datapoint
-        final_value;     %  Mean of last datapoint 
-        target_left;     %  Mean of datapoint after which the signal section starts 
-        target_right;    %  Mean of last datapoint of the signal section 
-        measurements;
-        %
-        noise_l;
-        noise_r;
-        noise_s;
+        start_value;    %  Mean of first datapoint
+        final_value;    %  Mean of last datapoint 
+        target_left;    %  Mean of datapoint after which the signal section starts 
+        target_right;   %  Mean of last datapoint of the signal section 
+        measurements;   %  #rows of X
     end
 
     properties (Constant)
-        
     end
     
-     methods
-         function create_signal(obj, app, noise_change)
-            
+    methods
+        function create_signal(obj, app, noise_change)
+        % Function to create the data ('Example') or load data for
+        % the 'LFP' and 'Paracetamol' case
+        % Example case: Creation of the data matrix X consisting of
+        % # of Experiments: rows
+        % # of Datapoints:  columns
+           
             switch obj.name
-                % Loads the respective data and creates the objects
-                % these objects will then later on be used
                 case 'Example'
-                   
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    % This function crestes the data matrix X consisting of
-                    % # of Experiments rows
-                    % # of Datapoints columns =
-                    % #"Uncorrleated points left" + #Sinal points + #"Uncorrelated points right")
-                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                     datapoints = app.DatapointsEditField.Value;
-                    data_points_signal = app.relevantDatapointsEditField.Value;
+                    data_points_signal = app.relDatapointsEditField.Value;
                     data_points_left = int64(app.PositionSingnalSlider.Value*(datapoints-data_points_signal));     
                     data_points_right = datapoints - (data_points_left+data_points_signal);
 
@@ -88,6 +79,7 @@ classdef Data < handle
                     obj.x_label_text = 'Wavenumber [cm^{-1}]';
                     obj.y_label_text = 'Absorbance';
                     obj.x_values = freq;
+
                 case 'LFP' 
                     if strcmp(obj.name, obj.active_dataset)==0 || noise_change
                         %% Load data 
@@ -104,85 +96,64 @@ classdef Data < handle
                     obj.y_label_text = '\DeltaQ_{100-10}';
                     obj.x_values = linspace(2.0,3.5,1000);
             end
-         end
+        end
 
-         function add_noise(obj, noise, noise_X, noise_y)
+        function add_noise(obj, noise, noise_X, noise_y)
+        % Adding noise to the data matrix X as well as y  
             if noise
                 obj.X = awgn(obj.X, noise_X, 'measured');
                 obj.y = awgn(obj.y, noise_y, 'measured');
             end
-         end 
+        end 
          
-         function u_lim = train_test_split(obj, fraction_train, new)
-            switch obj.name
-                % Loads the respective data and creates the objects
-                % these objects will then later on be used
-                case 'Example'
-                    % Train Test Split
-                    if new
-                        obj.idx_para = randperm(obj.measurements);
-                    end
-                    split_ind = int64(fraction_train*obj.measurements);
-                    u_lim = cast(split_ind-1, 'double');
-                    
-                    % Train Test Split
-                    %obj.X_train = obj.X(1:split_ind, :);
-                    %obj.X_test = obj.X(split_ind+1:end, :);
+        function u_lim = train_test_split(obj, fraction_train, new, group)
+        % splits the dataset into training and test dataset, accroding
+        % to the fraction. Respects the grouping structure if requested
+        % to do so for the 'Paracetamol' and 'LFP' dataset 
+            if strcmp(obj.name, 'LFP') || strcmp(obj.name, 'Paracetamol') && group
+                switch obj.name
+                    case 'Paracetamol'
+                        % Biased data in test dataset (default) 
+                        % 5 groups that are valid for training
+                        groups = 5;
+                    case 'LFP'
+                        % if biased data in test dataset (default) 
+                        % 5 groups that are valid for training
+                        groups = size(unique(obj.group),1);
+                end 
 
-                    %obj.y_train = obj.y(1:split_ind, :);
-                    %obj.y_test = obj.y(split_ind+1:end, :);
-                    idx_train = obj.idx_para(1:round(fraction_train*obj.measurements));
-                    idx_test = obj.idx_para(round(fraction_train*obj.measurements):end);
-                    
-                    obj.X_train = obj.X(idx_train, :);
-                    obj.X_test = obj.X(idx_test, :);
+                if new
+                    obj.idx_para = randperm(groups);
+                end
+                train_groups = obj.idx_para(1:ceil(fraction_train*groups));
 
-                    obj.y_train = obj.y(idx_train, :);
-                    obj.y_test = obj.y(idx_test, :);
-                case 'Paracetamol'
-                    %if biased data in test dataset (default) 
-                    % 5 groups that are valid for training
-                    groups = 5;
-                    if new
-                        obj.idx_para = randperm(groups);
-                    end
+                idx_test = sum(obj.group==train_groups,2)==0;
+                idx_train = sum(obj.group==train_groups,2)==1;
 
-                    train_groups = obj.idx_para(1:ceil(fraction_train*groups));
-                    
-                    idx_test = sum(obj.group==train_groups,2)==0;
-                    idx_train = sum(obj.group==train_groups,2)==1;
+                obj.X_train = obj.X(idx_train,:);
+                obj.X_test = obj.X(idx_test,:);
+                obj.y_train = obj.y(idx_train);
+                obj.y_test = obj.y(idx_test);
 
-                    obj.X_train = obj.X(idx_train,1:364);
-                    obj.X_test = obj.X(idx_test,1:364);
-
-                    obj.y_train = obj.y(idx_train);
-                    obj.y_test = obj.y(idx_test);
-                    
-                    u_lim = cast(size(obj.X_train,1)-1, 'double');
-                case 'LFP'
-                    % if biased data in test dataset (default) 
-                    % 5 groups that are valid for training
-                    groups = size(unique(obj.group),1);
-                    if new
-                        obj.idx_para = randperm(groups);
-                    end
-
-                    train_groups = obj.idx_para(1:ceil(fraction_train*groups));
-                    
-                    idx_test = sum(obj.group==train_groups,2)==0;
-                    idx_train = sum(obj.group==train_groups,2)==1;
-
-                    obj.X_train = obj.X(idx_train,:);
-                    obj.X_test = obj.X(idx_test,:);
-
-                    obj.y_train = obj.y(idx_train);
-                    obj.y_test = obj.y(idx_test);
-                    
-                    u_lim = cast(size(obj.X_train,1)-1, 'double');
+                u_lim = cast(size(obj.X_train,1)-1, 'double');
             end
-         end
-       
-             
+
+            if strcmp(obj.name, 'Example') || ~group
+                if new
+                    obj.idx_para = randperm(obj.measurements);
+                end
+                split_ind = int64(fraction_train*obj.measurements);
+                u_lim = cast(split_ind-1, 'double');
+
+                idx_train = obj.idx_para(1:round(fraction_train*obj.measurements));
+                idx_test = obj.idx_para(round(fraction_train*obj.measurements):end);
+
+                obj.X_train = obj.X(idx_train, :);
+                obj.X_test = obj.X(idx_test, :);
+
+                obj.y_train = obj.y(idx_train, :);
+                obj.y_test = obj.y(idx_test, :);
+            end
+        end
      end 
-    
 end 
